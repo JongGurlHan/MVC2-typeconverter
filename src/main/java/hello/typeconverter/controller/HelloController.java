23d0a209cd09dbb28382aba93b6af9be3d54cc4d
JG HAN <gojkhan13@gmail.com>
2021-09-14 01:15:08 +0900

    Initial commit

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request){
+        String data = request.getParameter("data"); //문자 타입 조회
+        Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
+        System.out.println("intValue = "+ intValue);
+        return "ok";
+
+        //Http요청 파라미터는 모두 문자 처리된다. 따라서 요청 파라미터를 자바에서 다른 타입으로 변환해서 사용하고 싶으면 숫자 타입으로 변환하는 과정을 거쳐야 한다.
+    }
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        System.out.println("data = " +data );
+        return "ok";
+    }
+
+}
